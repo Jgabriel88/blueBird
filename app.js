73d2eac2c42dd139b020d62aa3d2c521a9b29803
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-01 23:34:04 -0400

    created testendpoint to create a tweet in the database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Tweet = require('./modules/tweet');
 
 mongoose.connect('mongodb://localhost:27017/bluebird', {
 	useNewUrlParser: true,
@@ -12,6 +13,16 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	console.log('connected to the database');
+});
+
+app.get('/posttweet', async (req, res) => {
+	const tweet = new Tweet({
+		text: 'Test new tweet',
+		username: 'Gabriel',
+		likes: 25,
+	});
+	await tweet.save();
+	res.send(tweet);
 });
 
 app.listen(3000);
