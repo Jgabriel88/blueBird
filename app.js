d065f98c35b94285dfb171b6863e304104160a24
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-01 20:53:39 -0400

    intial setup mongoose

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/bluebird', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
 
 app.get('/', (req, res) => {
 	res.send('Connected to /');
