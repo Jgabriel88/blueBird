cfd90e0b56a18cc1245d180f54e9f7407f3fc2b7
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-01 23:20:37 -0400

    create modules structure

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const tweet = require('./api/tweet');
-const user = require('./api/user');
 const app = express();
+const mongoose = require('mongoose');
 
-app.use(bodyParser.json());
-app.use('/tweet', tweet);
-app.use('/user', user);
-
-mongoose.connect('mongodb://localhost:27017/twitter', {
+mongoose.connect('mongodb://localhost:27017/bluebird', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
@@ -19,10 +12,6 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	console.log('connected to the database');
-});
-
-app.get('/', (req, res) => {
-	res.send('Connected to /');
 });
 
 app.listen(3000);
