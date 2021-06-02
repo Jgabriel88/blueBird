af37aac2948a382e2ba82bcbc6681517fa336f3e
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-02 00:40:06 -0400

    async catch implemented to catch any error and send to express error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Tweet = require('./modules/tweet');
 const morgan = require('morgan');
+const asyncCatch = require('./helpers/AsyncCatch');
 
 mongoose.connect('mongodb://localhost:27017/bluebird', {
 	useNewUrlParser: true,
@@ -21,42 +22,53 @@ app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 
 //get a list of all tweets
-app.get('/tweets', async (req, res) => {
-	const tweets = await Tweet.find({});
-	res.send(`${tweets}`);
-});
+app.get(
+	'/tweets',
+	asyncCatch(async (req, res) => {
+		const tweets = await Tweet.find({});
+		res.send(`${tweets}`);
+	})
+);
 
 //get a specific tweet
-app.get('/tweets/:id', async (req, res, next) => {
-	try {
+app.get(
+	'/tweets/:id',
+	asyncCatch(async (req, res, next) => {
 		const { id } = req.params;
 		const tweet = await Tweet.findById(id);
 		res.send(tweet);
-	} catch (e) {
-		next(e);
-	}
-});
+	})
+);
 
 //create a new tweet
-app.post('/tweets', async (req, res) => {
-	const tweet = new Tweet(req.body);
-	await tweet.save();
-	res.send(tweet);
-});
+app.post(
+	'/tweets',
+	asyncCatch(async (req, res) => {
+		const tweet = new Tweet(req.body);
+		await tweet.save();
+		res.send(tweet);
+	})
+);
 
 //edit a tweet
-app.put('/tweets/:id', async (req, res) => {
-	const { id } = req.params;
-	const tweet = await Tweet.findByIdAndUpdate(id, { ...req.body });
-	res.send(tweet);
-});
+app.put(
+	'/tweets/:id',
+	asyncCatch(async (req, res) => {
+		const { id } = req.params;
+		const tweet = await Tweet.findByIdAndUpdate(id, { ...req.body });
+		res.send(tweet);
+	})
+);
 
 //delete a tweet
-app.delete('/tweets/:id', async (req, res) => {
-	const { id } = req.params;
-	await Tweet.findByIdAndDelete(id);
-	res.send(`Tweet id: ${id} DELETED!`);
-});
+app.delete(
+	'/tweets/:id',
+	asyncCatch(async (req, res) => {
+		const { id } = req.params;
+		await Tweet.findByIdAndDelete(id);
+		res.send(`Tweet id: ${id} DELETED!`);
+	})
+);
 
 app.use((err, req, res, next) => {
 	res.send('Something went wrong');
