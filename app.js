5ee67d72d1318ec76f7781f4176829bf8d54ee56
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-01 21:15:46 -0400

    mongodb connection with express server

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/twiter', {
+mongoose.connect('mongodb://localhost:27017/twiter', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+	console.log('connected to the database');
 });
 
 app.get('/', (req, res) => {
